ba07ccdb4fce7348d26c96ce324ab7b8ad03b7a6
Leah Somerville <leah.somerville4@gmail.com>
2024-03-04 09:46:08 -0600

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Leah Somerville on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
